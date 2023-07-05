b4a27bfd26eb21e2be2c1fd3589e5e847c51181a
nextwor1d <mobinsocial@gmail.com>
2023-07-05 17:15:21 +0330

    up

---
+++ lib/screens/count_down.screen.dart
@@ -0,0 +1,193 @@
+// ignore_for_file: avoid_print, use_key_in_widget_constructors, library_private_types_in_public_api
+
+import 'package:clock_pro/screens/rounded_button.dart';
+import 'package:flutter/material.dart';
+import 'package:stop_watch_timer/stop_watch_timer.dart';
+
+class CountDownTimerPage extends StatefulWidget {
+  static Future<void> navigatorPush(BuildContext context) async {
+    return Navigator.of(context).push<void>(
+      MaterialPageRoute(
+        builder: (_) => CountDownTimerPage(),
+      ),
+    );
+  }
+
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<CountDownTimerPage> {
+  final _isHours = true;
+
+  final StopWatchTimer _stopWatchTimer = StopWatchTimer(
+    mode: StopWatchMode.countDown,
+  );
+
+  final _scrollController = ScrollController();
+
+  @override
+  void dispose() async {
+    super.dispose();
+    await _stopWatchTimer.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Scrollbar(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 32,
+              horizontal: 16,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                /// Display stop watch time
+                StreamBuilder<int>(
+                  stream: _stopWatchTimer.rawTime,
+                  initialData: _stopWatchTimer.rawTime.value,
+                  builder: (context, snap) {
+                    final value = snap.data!;
+                    final displayTime =
+                        StopWatchTimer.getDisplayTime(value, hours: _isHours);
+                    return Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: Text(
+                            displayTime,
+                            style: const TextStyle(
+                                fontSize: 40,
+                                fontFamily: 'Helvetica',
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+
+                /// Lap time.
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: SizedBox(
+                    height: 100,
+                    child: StreamBuilder<List<StopWatchRecord>>(
+                      stream: _stopWatchTimer.records,
+                      initialData: _stopWatchTimer.records.value,
+                      builder: (context, snap) {
+                        final value = snap.data!;
+                        if (value.isEmpty) {
+                          return const SizedBox();
+                        }
+                        Future.delayed(const Duration(milliseconds: 100), () {
+                          _scrollController.animateTo(
+                              _scrollController.position.maxScrollExtent,
+                              duration: const Duration(milliseconds: 200),
+                              curve: Curves.easeOut);
+                        });
+                        print('Listen records. $value');
+                        return ListView.builder(
+                          controller: _scrollController,
+                          scrollDirection: Axis.vertical,
+                          itemBuilder: (BuildContext context, int index) {
+                            final data = value[index];
+                            return Column(
+                              children: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.all(8),
+                                  child: Text(
+                                    '${index + 1} ${data.displayTime}',
+                                    style: const TextStyle(
+                                        fontSize: 17,
+                                        fontFamily: 'Helvetica',
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                const Divider(
+                                  height: 1,
+                                )
+                              ],
+                            );
+                          },
+                          itemCount: value.length,
+                        );
+                      },
+                    ),
+                  ),
+                ),
+
+                /// Button
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 4),
+                        child: RoundedButton(
+                          color: Colors.lightBlue,
+                          onTap: _stopWatchTimer.onStartTimer,
+                          child: const Text(
+                            'Start',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 4),
+                        child: RoundedButton(
+                          color: Colors.green,
+                          onTap: _stopWatchTimer.onStopTimer,
+                          child: const Text(
+                            'Stop',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 4),
+                        child: RoundedButton(
+                          color: Colors.red,
+                          onTap: _stopWatchTimer.onResetTimer,
+                          child: const Text(
+                            'Reset',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 4),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Padding(
+                              padding:
+                                  const EdgeInsets.all(0).copyWith(right: 8),
+                              child: RoundedButton(
+                                color: Colors.deepPurpleAccent,
+                                onTap: _stopWatchTimer.onAddLap,
+                                child: const Text(
+                                  'Lap',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
